3abb05b9f473938e04ca610a12db4887a1a451d5
seongwookchun <40543038+seongwookchun@users.noreply.github.com>
2020-01-01 20:21:43 +0900

    missing self obj

--- k2sc_tools/utils/cust_pd.py
+++ k2sc_tools/utils/cust_pd.py
@@ -4,7 +4,7 @@ class Cust_pd():
     def __init__(self, var_df_base):
         self.df_base = var_df_base
 
-    def f_keyword_compare(var_keyword, var_col_label='smishing', var_col_text='text'):
+    def f_keyword_compare(self, var_keyword, var_col_label='smishing', var_col_text='text'):
         print('*'*100)
         print('smishing')
         print(df_base[(df_base[var_col_label]==1) & (df_base[var_col_text].str.contains(var_keyword))][var_col_text])
