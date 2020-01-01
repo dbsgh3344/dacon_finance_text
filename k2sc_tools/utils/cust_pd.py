c57cce2a850230bf0569400f3d164645b59fe14b
seongwookchun <40543038+seongwookchun@users.noreply.github.com>
2020-01-01 20:08:59 +0900

    typo

--- k2sc_tools/utils/cust_pd.py
+++ k2sc_tools/utils/cust_pd.py
@@ -7,7 +7,7 @@ class Cust_pd():
     def f_keyword_compare(var_keyword, var_col_label='smishing', var_col_text='text'):
         print('*'*100)
         print('smishing')
-        print(df_base[(df_base[var_col_label]==1) & (df_base[var_col_text'].str.contains(var_keyword))][var_col_text'])
+        print(df_base[(df_base[var_col_label]==1) & (df_base[var_col_text'].str.contains(var_keyword))][var_col_text])
         print('*'*100)
         print('normal text')
-        print(df_base[(df_base[var_col_label]==0) & (df_base[var_col_text'].str.contains(var_keyword))][var_col_text'])
+        print(df_base[(df_base[var_col_label]==0) & (df_base[var_col_text'].str.contains(var_keyword))][var_col_text])
