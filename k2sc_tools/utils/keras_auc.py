8b47ad89142880bc805d8fc9d47c7a3a5e080ce3
seongwookchun <40543038+seongwookchun@users.noreply.github.com>
2020-01-03 14:46:41 +0900

    Update keras_auc.py

--- k2sc_tools/utils/keras_auc.py
+++ k2sc_tools/utils/keras_auc.py
@@ -1,5 +1,5 @@
 import keras.backend as K
-from keras.callbacks import LambdaCallback
+
 def f_f1_score(y_true, y_pred):
     # print(y_true)#, y_pred)
     # print(y_pred)
@@ -33,3 +33,29 @@ def f_f1_score(y_true, y_pred):
     f1 = 2*((precision*recall)/(precision+recall+K.epsilon()))
 
     return f1
+
+def recall(y_true, y_pred):
+    """Recall metric.
+
+    Only computes a batch-wise average of recall.
+
+    Computes the recall, a metric for multi-label classification of
+    how many relevant items are selected.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision(y_true, y_pred):
+    """Precision metric.
+
+    Only computes a batch-wise average of precision.
+
+    Computes the precision, a metric for multi-label classification of
+    how many selected items are relevant.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
